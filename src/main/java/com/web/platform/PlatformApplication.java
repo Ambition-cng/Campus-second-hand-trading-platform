c7db42780e607171ff5a2d129f976f8d6b3659b7
cddorz <2878153350@qq.com>
2022-05-06 21:12:36 +0800

    init

---
+++ src/main/java/com/web/platform/PlatformApplication.java
@@ -0,0 +1,18 @@
+package com.web.platform;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hly
+ * @Description: TODO
+ * @create 2022-05-06 21:10
+ */
+@SpringBootApplication
+@MapperScan("com.web.platform.mapper")
+public class PlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+}
